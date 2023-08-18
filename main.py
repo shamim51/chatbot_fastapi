23d7742a225e196085584785ec9cd03985c1c68d
Shamim Ahmed <shamimahmed30082@gmail.com>
2023-08-19 01:25:46 +0600

    added predict route

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,10 +11,23 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+class Message(BaseModel):
+    message: str
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.post("/predict")
+async def predict(message: Message):
+    try:
+        print(message.message)
+        #print(Message)
+        response = {"answer": f"This is a dummy response to: '{message.message}'"}
+        return response
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 
