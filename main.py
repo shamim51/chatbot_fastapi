ee715ebfb6708166ce8bc5b75e74c13e23445656
Shamim Ahmed <shamimahmed30082@gmail.com>
2023-08-19 01:11:14 +0600

    added gitignore

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-
-from app.functions.genResponse import genResponse
-
-
 
 app = FastAPI()
 
-# Configure CORS to allow cross-origin requests
-origins = ["*"]  # Update this list to restrict access to specific domains
+origins = ["*"]  
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -17,21 +11,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class Message(BaseModel):
-    message: str
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
 
-
-@app.post("/predict")
-async def predict(message: Message):
-    try:
-        print(message.message)
-        #print(Message)
-        #response = {"answer": f"This is a dummy response to: '{message.message}'"}
-        response = genResponse(message.message)
-        return response
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
