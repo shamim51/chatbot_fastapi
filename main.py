4190a4e5bece181ed816f73f0d166eba8b4a1ab1
Shamim Ahmed <shamimahmed30082@gmail.com>
2023-08-19 08:51:06 +0600

    changed port

--- main.py
+++ main.py
@@ -33,6 +33,6 @@ async def predict(message: Message):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
 
     
